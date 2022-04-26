a6c10e5c579fdb6631f680febd5e5c39bb5b3a11
JackyPinkgin <dangkky@gmail.com>
2022-04-26 19:36:36 +0800

    完成86的作业->完成department的删除和更新接口的内容

--- src/main/java/com/jacky/wework/contact/Department.java
+++ src/main/java/com/jacky/wework/contact/Department.java
@@ -4,6 +4,8 @@ import com.jacky.wework.Wework;
 import com.jayway.jsonpath.JsonPath;
 import io.restassured.response.Response;
 
+import java.io.*;
+
 import static io.restassured.RestAssured.given;
 
 /**
@@ -31,6 +33,42 @@ public class Department {
                 .when().body(body)
                 .post("https://qyapi.weixin.qq.com/cgi-bin/department/create")
                 .then().log().all().extract().response();
+    }
+
+    //用于个人使用的 创建部门方法
+    public Response create1(String name, int parentid) {
+        String body = JsonPath.parse(this.getClass().getResourceAsStream("/data/create1.json"))
+                .set("$.name", name)
+                .set("$.parentid", parentid)
+                .jsonString();
+
+        return given().log().all().queryParam("access_token", Wework.getToken())
+                .when().body(body)
+                .post("https://qyapi.weixin.qq.com/cgi-bin/department/create")
+                .then().log().all().extract().response();
+    }
+
+    public Response delete(String id) {
+        return given().log().all()
+                .param("access_token", Wework.getToken()).param("id", id)
+                .when()
+                .get("https://qyapi.weixin.qq.com/cgi-bin/department/delete")
+                .then().log().all()
+                .statusCode(200)
+                .extract().response();
+    }
+
+    public Response update(int id, int parentid){
+
+        String body = JsonPath.parse(this.getClass().getResourceAsStream("/data/update.json"))
+                .set("$.id", id)
+                .set("$.parentid", parentid).jsonString();
+
+        return given().log().all()
+                .queryParam("access_token",Wework.getToken())
+                .when().body(body)
+                .post("https://qyapi.weixin.qq.com/cgi-bin/department/update")
+                .then().log().all().statusCode(200).extract().response();
     }
 
 }
--- src/test/java/com/jacky/wework/contact/DepartmentTest.java
+++ src/test/java/com/jacky/wework/contact/DepartmentTest.java
@@ -23,14 +23,37 @@ class DepartmentTest {
         department.list("").then().statusCode(200)
                 .body("department.name", hasItem("Off_white"))
                 .body("department[0].id", equalTo(1));
-        department.list("8").then().statusCode(200)
-                .body("department.name", hasItem("研发部"))
-                .body("department.id[1]", equalTo(9));
+//        department.list("8").then().statusCode(200)
+//                .body("department.name", hasItem("研发部"))
+//                .body("department.id[1]", equalTo(9));
     }
 
     @Test
     void create() {
         department.create("AppDevDept_1","8");
         department.create("AppDevDept_1","8").then().body("errcode",equalTo(60008));
+    }
+
+    @Test
+    void create1() {
+        department.create1("HuDongYuLeBu",8).then().body("errmsg",equalTo("created"));
+    }
+
+    @Test
+    void delete() {
+        //先增加
+        department.create1("HuDongYuLeBu",8)
+                .then().body("errmsg",equalTo("created"));
+        //再删除
+        department.delete("34").then()
+                .body("errcode",equalTo(0))
+                .body("errmsg",equalTo("deleted"));
+    }
+
+
+    @Test
+    void update() {
+        department.update(33,9).then()
+                .body("errmsg",equalTo("updated"));
     }
 }
